489c21af851e4174e0c0765a1539fe0aee757978
William Okou <gokou097@gmail.com>
2023-07-06 10:26:49 +0100

    route removed due to incompatibility

--- src/app/apis/moralisss/[...moralis]/route.ts
+++
@@ -1,7 +0,0 @@
-// 'use client';
-
-import {MoralisNextApi} from '@moralisweb3/next';
-
-export default MoralisNextApi({apiKey: process.env.MORALIS_API_KEY});
-
-// export {handler as GET, handler as POST};
